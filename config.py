52530a4a7742a6e1afb5881299c5334276bdcee2
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-02-25 15:28:20 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#Necessary Variables 
+API_ID = int(getenv("API_ID", ))
+API_HASH = getenv("API_HASH", )
+BOT_TOKEN = getenv("BOT_TOKEN") #Put your bot token here
+LOG_ID = int(getenv("LOG_ID"))
+SUDOERS = list(map(int, getenv("SUDOERS", "").split()))
