6e9dc9894ac4ac80404cadc69ab879285b1fe8eb
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-02-25 17:33:57 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ from pyrogram import Client, filters
 from pyrogram.errors import PeerIdInvalid, ChannelInvalid, FloodWait
 from pyrogram.types import BotCommand
 
-from config import API_ID, API_HASH, BOT_TOKEN, LOG_ID
+from config import API_ID, API_HASH, BOT_TOKEN
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
     level=logging.INFO,
@@ -27,19 +27,9 @@ boot = time.time()
 async def eval_bot():
     try:
         await app.start()
+        print(1)
     except FloodWait as ex:
         LOGGER.warning(ex)
         await asyncio.sleep(ex.value)
-    print(1)
-    try:
-        await app.send_message(chat_id=LOG_ID, text=f"<u><b>» {app.me.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{app.me.id}</code>\nɴᴀᴍᴇ : {app.me.first_name}\nᴜsᴇʀɴᴀᴍᴇ : @{app.me.username}")
-        LOGGER.info(f"Bot Started As {app.me.first_name}")
-    except Exception as e:
-        print(e)
-        exit()
-    try:
-        await app.set_bot_commands([BotCommand("start", "Starts The Bot")])
-    except Exception as e:
-        print(f"Cmds {e}")
-
+    print(2)
 asyncio.get_event_loop().run_until_complete(eval_bot())
