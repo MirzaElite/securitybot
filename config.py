1724c0e006bd0f446cbabd57d8c8728a510dadec
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2024-02-25 17:36:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #Necessary Variables 
-API_ID = int(getenv("API_ID", ))
-API_HASH = getenv("API_HASH", )
-BOT_TOKEN = getenv("BOT_TOKEN") #Put your bot token here
-LOG_ID = int(getenv("LOG_ID"))
-SUDOERS = list(map(int, getenv("SUDOERS", "").split()))
+API_ID = int(getenv("API_ID", "29400566"))
+API_HASH = getenv("API_HASH", "8fd30dc496aea7c14cf675f59b74ec6f")
+BOT_TOKEN = getenv("BOT_TOKEN",) #Put your bot token here
