548dab38982734b1d05ace8fd5e58745ae5a4d57
MirzaElite <126695903+MirzaElite@users.noreply.github.com>
2024-02-25 18:17:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 #Necessary Variables 
 API_ID = int(getenv("API_ID", "29400566"))
 API_HASH = getenv("API_HASH", "8fd30dc496aea7c14cf675f59b74ec6f")
-BOT_TOKEN = getenv("BOT_TOKEN",) #Put your bot token here
+BOT_TOKEN = getenv("BOT_TOKEN", "7124170275:AAFzQl25c2MVQLACM8DZz7tPOvU6RscnSrk") #Put your bot token here
